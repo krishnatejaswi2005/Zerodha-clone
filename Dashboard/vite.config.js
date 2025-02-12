9c21f0816a3360672dec4db3be99ab7dd6b681b8
krishnatejaswi2005 <tejaswi.jgs@gmail.com>
2025-02-13 00:01:56 +0530

    Removed URL malformed error

--- Dashboard/vite.config.js
+++ Dashboard/vite.config.js
@@ -4,10 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	server: {
-		hmr: {
-			overlay: false, // Disables the error overlay
-		},
-		allowedHosts: ["6fe8-103-161-31-178.ngrok-free.app"], // Add your host here
-	},
 });
