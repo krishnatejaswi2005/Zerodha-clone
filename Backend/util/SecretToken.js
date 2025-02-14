2aa4300b8fbf5982bf47bb585a736bbcbf29351f
krishnatejaswi2005 <tejaswi.jgs@gmail.com>
2025-02-14 09:56:34 +0530

    Edited secret token

--- Backend/util/SecretToken.js
+++ Backend/util/SecretToken.js
@@ -2,7 +2,12 @@ require("dotenv").config();
 const jwt = require("jsonwebtoken");
 
 module.exports.createSecretToken = (id, username) => {
-	return jwt.sign({ id, username }, process.env.TOKEN_KEY, {
-		expiresIn: 3 * 24 * 60 * 60,
-	});
+	try {
+		return jwt.sign({ id, username }, process.env.TOKEN_KEY, {
+			expiresIn: 3 * 24 * 60 * 60,
+		});
+	} catch (error) {
+		console.error("Error creating token:", error);
+		return null;
+	}
 };
